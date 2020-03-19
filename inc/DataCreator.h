b4f2fac9b8f4cd19527e97c89c70bab1e0f950f1
Michael Gordon <mgordon6900@conestogac.on.ca>
2020-03-19 14:50:10 -0400

    Create DataCreator.h

---
+++ inc/DataCreator.h
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/sem.h>
+#include <sys/wait.h>
+#include <sys/sem.h>
+#include <sys/stat.h>
+#include <time.h>
+
+
+
+#define MESSAGE_LENGTH  100
+#define MACHINE_ON      1
+#define EVERYTHING_OKAY 0
+#define KEY             1337
+#define LOG_FOLDER_PATH "/tmp/"
+#define LOG_FILE_PATH   "/tmp/dataCreator.log"
+#define FALSE           0
+#define TRUE            1
+
+
+typedef struct Messages{
+  long type;
+  int id;
+  int status;
+} MachineMessage;
+
+void sendMessage(int status, int machineID, long messageType, int msgQueueID);
+void machineProcessingLoop(void);
+void writeToLog(int pid, int status);
+void createFilePathIfNotExists(void);
+void getTime(char* output);
+int checkForQueue(key_t msgKey);
+
+#ifndef __SEMAPHORE_STRUCT_H__
+#define __SEMAPHORE_STRUCT_H__
+
+struct sembuf acquire_operation = { 0, -1, SEM_UNDO };
+struct sembuf release_operation = { 0, 1, SEM_UNDO };
+
+unsigned short init_values[1] = { 1 };
+
+#else
+
+extern struct sembuf acquire_operation;
+extern struct sembuf release_operation;
+
+#endif
